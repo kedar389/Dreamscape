90e316324af360ad4e7ff7131eb9556134f0ce1f
Filip Hemlézi <filip.hemlezi@student.tuke.sk>
2023-04-15 03:04:00 +0200

    Complete player physics rewrite (tweaks needed)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,107 +4,140 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Transform character;
+	public Transform character;
 
-    public List<Transform> availableCharacters;
-    public List<GameObject> availableCameras;
+	public List<Transform> availableCharacters;
+	public List<GameObject> availableCameras;
 
-    public const float maxVelocity = 8f;
-    public const float acceleration = 0.3f;
-	public float velocity = 0;
-    public float movingDirection = 0;
+	public const float maxHorizontalVelocity = 8f;
+	public const float maxVerticalVelocity = 30f;
+	public const float acceleration = 0.1f;
+	public const float gravity = 0.2f;
+
+	public float horizontalVelocity = 0;
+	public float verticalVelocity = 0;
+	public float movingDirection = 0;
 
 	public int whichCharacter;
-    public float jumpForce = 10f;
+	public float jumpForce = 40f; 
 
-    public LayerMask groundLayer;
-    private BoxCollider2D coll;
-    private Rigidbody2D rb;
-    private bool isGrounded;
+	public LayerMask groundLayer;
+	private BoxCollider2D coll;
+	private Rigidbody2D rb;
+	private bool isGrounded;
 
-
-    void Start()
-    {
-        if (character == null && availableCharacters.Count > 1)
-        {
-            character = availableCharacters[0];
-            availableCameras[0].SetActive(true);
-        }
-        SwitchBodies();
-        rb = GetComponent<Rigidbody2D>();
-        coll = GetComponent<BoxCollider2D>();
-
-    }
-
-    void Update()
-    {
-        float move = Input.GetAxisRaw("Horizontal");
-
-        if (move == 0 && velocity > 0) 
-        {
-			velocity -= acceleration / 3;
+	void Start()
+	{
+		if (character == null && availableCharacters.Count > 1)
+		{
+			character = availableCharacters[0];
+			availableCameras[0].SetActive(true);
 		}
-        else if(move == movingDirection) 
-        {
-            if (velocity < maxVelocity)
-            {
-                velocity += acceleration;
-            }
-        }
-        else if(move!=movingDirection && velocity>0)
-        {
-            velocity -= acceleration/3;
-        }
-        else if(velocity<=0)
-        {
-            movingDirection = move;
-        }
-        
+		SwitchBodies();
+		rb = GetComponent<Rigidbody2D>();
+		coll = GetComponent<BoxCollider2D>();
+		rb.isKinematic = true; // Disable physics for the Rigidbody2D
+	}
 
-
-        rb.velocity = new Vector2(movingDirection * velocity, rb.velocity.y);
+	void Update()
+	{
+		float move = Input.GetAxisRaw("Horizontal");
 
 		Vector2 position = transform.position;
 		Vector2 direction = Vector2.down;
 		float distance = coll.bounds.extents.y + 0.1f;
 		isGrounded = Physics2D.Raycast(position, direction, distance, groundLayer);
 
+		bool isBlockedLeft=Physics2D.Raycast(position,Vector2.left,coll.bounds.extents.x-0.1f,groundLayer);
+		bool isBlockedRight = Physics2D.Raycast(position, Vector2.right, coll.bounds.extents.x + 0.1f, groundLayer);
+		bool isBlockedTop = Physics2D.Raycast(position, Vector2.up, coll.bounds.extents.y - 0.1f, groundLayer);
+
+		if (!isGrounded)
+		{
+			verticalVelocity -=gravity;
+		}
+		else if (isGrounded)
+		{
+			verticalVelocity = 0;
+		}
+		else if (isBlockedTop)
+		{
+			verticalVelocity = 0;
+		}
+
+
+		if (horizontalVelocity < 0)
+		{
+			horizontalVelocity = 0;
+		}
+
+		if (horizontalVelocity <= 0 && move != movingDirection)
+		{
+			movingDirection = move;
+		}
+
+		if (move == 0 && horizontalVelocity > 0)
+		{
+			horizontalVelocity -= acceleration / 3;
+		}
+		else if (move == movingDirection)
+		{
+			if (horizontalVelocity < maxHorizontalVelocity)
+			{
+				horizontalVelocity += acceleration;
+			}
+		}
+		else if (move != movingDirection && horizontalVelocity > 0)
+		{
+			horizontalVelocity -= acceleration / 3;
+		}
+
+		if(movingDirection<0 && isBlockedLeft)
+		{
+			horizontalVelocity = 0;
+		}
+
+		if (movingDirection > 0 && isBlockedRight)
+		{
+			horizontalVelocity = 0;
+		}
+
+
 		if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
+		{
+			// Update vertical movement using transform instead of Rigidbody2D's velocity
+			verticalVelocity += jumpForce; 
+		}
 
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            if (whichCharacter == availableCharacters.Count - 1)
-            {
-                whichCharacter = 0;
+		// Update horizontal movement using transform instead of Rigidbody2D's velocity
+		transform.position += new Vector3(movingDirection * horizontalVelocity * Time.deltaTime, verticalVelocity * Time.deltaTime, 0);
 
-            }
-            else
-            {
-                whichCharacter += 1;
+		if (Input.GetKeyDown(KeyCode.Tab))
+		{
+			if (whichCharacter == availableCharacters.Count - 1)
+			{
+				whichCharacter = 0;
+			}
+			else
+			{
+				whichCharacter += 1;
+			}
+			SwitchBodies();
+		}
+	}
 
-            }
-            SwitchBodies();
-        } 
-    }
-
-
-    void SwitchBodies()
-    {
-        character = availableCharacters[whichCharacter];
-        character.GetComponent<PlayerController>().enabled = true;
-        availableCameras[whichCharacter].SetActive(true);
-        for (int i = 0; i < availableCharacters.Count; i++)
-        {
-            if (availableCharacters[i] != character)
-            {
-                availableCharacters[i].GetComponent<PlayerController>().enabled = false;
-                availableCameras[i].SetActive(false);
-            }
-
-        }
-
-    }
+	void SwitchBodies()
+	{
+		character = availableCharacters[whichCharacter];
+		character.GetComponent<PlayerController>().enabled = true;
+		availableCameras[whichCharacter].SetActive(true);
+		for (int i = 0; i < availableCharacters.Count; i++)
+		{
+			if (availableCharacters[i] != character)
+			{
+				availableCharacters[i].GetComponent<PlayerController>().enabled = false;
+				availableCameras[i].SetActive(false);
+			}
+		}
+	}
 }
