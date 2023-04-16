d96fb5a9b1b2401469460acb695641b0a743b927
kedar389 <kedar389@gmail.com>
2023-04-16 02:31:39 +0200

    last push

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -2,75 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FallingPlatform : MonoBehaviour
 {
     public Rigidbody2D rb;
     public Vector3 basePosition;
     public float time_until_fall = 3;
     public int speed = 10;
-    private bool donecollision;
+    private bool isFalling = false;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        basePosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        basePosition = transform.position;
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.tag.Equals("Player") || col.gameObject.tag.Equals("Nightmare"))
+        if (col.gameObject.CompareTag("Nightmare") || col.gameObject.CompareTag("Enemy"))
         {
-            Invoke("DropPlatform", time_until_fall);
-            //Destroy(gameObject, 2f);
-
+            Debug.Log("collided");
+            StartCoroutine(DropPlatform());
         }
-
-
     }
 
-
-    private void OnCollisionStay2D(Collision2D other)
+    IEnumerator DropPlatform()
     {
-        if (other.gameObject.tag.Equals("Nightmare"))
+        yield return new WaitForSeconds(time_until_fall);
+        if (!isFalling)
         {
-            donecollision = false;
-
+            isFalling = true;
+            rb.bodyType = RigidbodyType2D.Dynamic;
         }
-
     }
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.tag.Equals("Nightmare"))
-        {
-            donecollision = true;
 
-        }
-
-    }
     void Update()
     {
-        if (rb.velocity.y == 0 && transform.position != basePosition)
+        if (rb.velocity.y == 0 && transform.position != basePosition && isFalling)
         {
-            if (donecollision)
-            {
-                Invoke("RaisePlatform", 5f);
-            }
+            StartCoroutine(RaisePlatform());
         }
-
-
-
-    }
-    // Update is called once per frame
-    void DropPlatform()
-    {
-        rb.bodyType = RigidbodyType2D.Dynamic;
-
     }
 
-    void RaisePlatform()
+    IEnumerator RaisePlatform()
     {
+        isFalling = false;
         rb.bodyType = RigidbodyType2D.Kinematic;
-        transform.position = Vector3.MoveTowards(transform.position, (basePosition), speed * Time.deltaTime);
-
+        while (transform.position != basePosition)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, basePosition, speed * Time.deltaTime);
+            yield return null;
+        }
+        rb.velocity = Vector2.zero;
+        rb.bodyType = RigidbodyType2D.Static;
     }
 }
